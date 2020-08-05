12eabea536f52e1f0f4f5101c75ad120a0d4439c
langdon <langdon@fedoraproject.org>
2020-08-05 15:04:05 -0400

    adding the blender-render script and container

---
+++ S1E2/blender-render/blender-render.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+display_usage() {
+	echo -e "\nUsage:\n$0 BLEND_FILE (assumes to add 'blend') \n"
+}
+
+# if less than one argument supplied, display usage
+if [  $# -le 0 ]
+then
+    display_usage
+    exit 1
+fi
+
+echo "Starting `date`"
+/opt/blender*/blender -b $1.blend -t 0 -x 1 -o //$1_render -a
+echo "Completed render at `date`"
+
